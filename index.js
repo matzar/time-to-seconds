627ead7dafc413692c8b95547ff6d0e38abddcb5
Mateusz Zaremba <mat.zar@icloud.com>
2023-09-05 10:28:49 +0100

    fix: update timeArray validation to handle non-numeric strings (#49)
    * fix: update timeArray validation to handle non-numeric strings
    
    * chore: rename function argument: seconds to timeString

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 "use strict";
 
-module.exports = function (seconds) {
-  let timeArray = seconds.split(":");
+module.exports = function (timeString) {
+  let timeArray = timeString.split(":");
 
-  if (timeArray.some(isNaN))
+  if (timeArray.some((val) => Number.isNaN(Number(val))))
     throw new TypeError(
       'time-to-seconds: wrong argument type - something else than a number string in format "number", "number:number" or "number:number:number" was passed. See documentation for more information on argument formatting: https://www.npmjs.com/package/time-to-seconds.'
     );
